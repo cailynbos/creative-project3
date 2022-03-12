e28c34ab662e679baa52cfedebc4ee949a1ec31c
Cailyn Bosworth <cailyn.bosworth@gmail.com>
2022-03-12 16:25:45 -0700

    create header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <div id="header">
+      <h1>Book Recs</h1>
+      <nav>
+        <router-link to="/">All Books</router-link> |
+        <router-link to="/scifi">Sci-fi</router-link> |
+        <router-link to="/fantasy">Fantasy</router-link>
+      </nav>
+    </div>
     <router-view/>
     <div id="footer">
       <a href="https://github.com/cailynbos/creative-project3">GitHub link</a>
@@ -12,28 +16,56 @@
 </template>
 
 <style>
+html, body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #393d3f;
+}
+
+#header {
+  background-color: #c6c5b9;
   text-align: center;
-  color: #2c3e50;
+}
+
+#header h1 {
+  margin-top: 0px;
+  padding-top: 20px;
 }
 
 nav {
-  padding: 30px;
+  margin: 0px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #393d3f;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #62929e;
 }
 
 #footer {
   text-align: left;
+  background-color: #c6c5b9;
+  height: 20px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 10px;
+}
+
+#footer a {
+  color: #393d3f;
+  text-decoration: none;
 }
 </style>
