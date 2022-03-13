dbb919f9ccab9b78481b4db18b90142198e9132b
Cailyn Bosworth <cailyn.bosworth@gmail.com>
2022-03-12 17:18:41 -0700

    add data

---
+++ src/data.js
@@ -0,0 +1,122 @@
+let mock = [{
+    title: "Ender's Game",
+    author: "Orson Scott Card",
+    image: "endersgame.jpg",
+    genre: "scifi"
+},
+{
+    title: "Dune",
+    author: "Frank Herbert",
+    image: "dune.jpg",
+    genre: "scifi"
+},
+{
+    title: "1984",
+    author: "George Orwell",
+    image: "1984.jpg",
+    genre: "scifi"
+},
+{
+    title: "The Man in the High Castle",
+    author: "Philip K. Dick",
+    image: "man-in-the-high-castle.jpg",
+    genre: "scifi"
+},
+{
+    title: "Frankenstein",
+    author: "Mary Shelley",
+    image: "frankenstein.jpg",
+    genre: "scifi"
+},
+{
+    title: "The Moon is a Harsh Mistress",
+    author: "Robert A. Heinlein",
+    image: "moon-is-a-harsh-mistress.jpg",
+    genre: "scifi"
+}, 
+{
+    title: "Children of Time",
+    author: "Adrian Tchaikovsky",
+    image: "children-of-time.jpg",
+    genre: "scifi"
+},
+{
+    title: "The Martian",
+    author: "Andy Weir",
+    image: "martian.jpg",
+    genre: "scifi"
+},
+{
+    title: "The Heart Goes Last",
+    author: "Margaret Atwood",
+    image: "heart-goes-last.jpg",
+    genre: "scifi"
+},
+{
+    title: "The Three-Body Problem",
+    author: "Cixin Liu",
+    image: "three-body-problem.jpg",
+    genre: "scifi"
+},
+{
+    title: "Mistborn",
+    author: "Brandon Sanderson",
+    image: "mistborn.jpg",
+    genre: "fantasy"
+},
+{
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    image: "harry-potter.jpg",
+    genre: "fantasy"
+},
+{
+    title: "The Way of Kings",
+    author: "Brandon Sanderson",
+    image: "way-of-kings.jpg",
+    genre: "fantasy"
+},
+{
+    title: "A Game of Thrones",
+    author: "George R. R. Martin",
+    image: "game-of-thrones.jpg",
+    genre: "fantasy"
+},
+{
+    title: "Shadow and Bone",
+    author: "Leigh Bardugo",
+    image: "shadow-and-bone.jpg",
+    genre: "fantasy"
+},
+{
+    title: "The Night Circus",
+    author: "Erin Morgenstern",
+    image: "night-circus.jpg",
+    genre: "fantasy"
+},
+{
+    title: "Stardust",
+    author: "Neil Gaiman",
+    image: "stardust.jpg",
+    genre: "fantasy"
+},
+{
+    title: "The Eye of the World",
+    author: "Robert Jordan",
+    image: "eye-of-world.jpg",
+    genre: "fantasy"
+},
+{
+    title: "The Lion, the Witch, and the Wardrobe",
+    author: "C.S. Lewis",
+    image: "narnia.jpg",
+    genre: "fantasy"
+},
+{
+    title: "Jonathan Strange & Mr. Norrell",
+    author: "Susanna Clarke",
+    image: "jonathan-strange.jpg",
+    genre: "fantasy"
+}]
+
+export default mock;
