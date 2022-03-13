6de5f23a48e9e8ef2ee87af0022cd273107ac487
Cailyn Bosworth <cailyn.bosworth@gmail.com>
2022-03-12 18:59:42 -0700

    fixed data

--- src/data.js
+++ src/data.js
@@ -1,118 +1,138 @@
 let mock = [{
+    id: 1,
     title: "Ender's Game",
     author: "Orson Scott Card",
     image: "endersgame.jpg",
     genre: "scifi"
 },
 {
+    id: 2,
     title: "Dune",
     author: "Frank Herbert",
     image: "dune.jpg",
     genre: "scifi"
 },
 {
+    id: 3,
     title: "1984",
     author: "George Orwell",
     image: "1984.jpg",
     genre: "scifi"
 },
 {
+    id: 4,
     title: "The Man in the High Castle",
     author: "Philip K. Dick",
     image: "man-in-the-high-castle.jpg",
     genre: "scifi"
 },
 {
+    id: 5,
     title: "Frankenstein",
     author: "Mary Shelley",
     image: "frankenstein.jpg",
     genre: "scifi"
 },
 {
+    id: 6,
     title: "The Moon is a Harsh Mistress",
     author: "Robert A. Heinlein",
     image: "moon-is-a-harsh-mistress.jpg",
     genre: "scifi"
 }, 
 {
+    id: 7,
     title: "Children of Time",
     author: "Adrian Tchaikovsky",
     image: "children-of-time.jpg",
     genre: "scifi"
 },
 {
+    id: 8,
     title: "The Martian",
     author: "Andy Weir",
     image: "martian.jpg",
     genre: "scifi"
 },
 {
+    id: 9,
     title: "The Heart Goes Last",
     author: "Margaret Atwood",
     image: "heart-goes-last.jpg",
     genre: "scifi"
 },
 {
+    id: 10,
     title: "The Three-Body Problem",
     author: "Cixin Liu",
     image: "three-body-problem.jpg",
     genre: "scifi"
 },
 {
+    id: 11,
     title: "Mistborn",
     author: "Brandon Sanderson",
     image: "mistborn.jpg",
     genre: "fantasy"
 },
 {
+    id: 12,
     title: "Harry Potter and the Sorcerer's Stone",
     author: "J.K. Rowling",
     image: "harry-potter.jpg",
     genre: "fantasy"
 },
 {
+    id: 13,
     title: "The Way of Kings",
     author: "Brandon Sanderson",
     image: "way-of-kings.jpg",
     genre: "fantasy"
 },
 {
+    id: 14,
     title: "A Game of Thrones",
     author: "George R. R. Martin",
     image: "game-of-thrones.jpg",
     genre: "fantasy"
 },
 {
+    id: 15,
     title: "Shadow and Bone",
     author: "Leigh Bardugo",
     image: "shadow-and-bone.jpg",
     genre: "fantasy"
 },
 {
+    id: 16,
     title: "The Night Circus",
     author: "Erin Morgenstern",
     image: "night-circus.jpg",
     genre: "fantasy"
 },
 {
+    id: 17,
     title: "Stardust",
     author: "Neil Gaiman",
     image: "stardust.jpg",
     genre: "fantasy"
 },
 {
+    id: 18,
     title: "The Eye of the World",
     author: "Robert Jordan",
     image: "eye-of-world.jpg",
     genre: "fantasy"
 },
 {
+    id: 19,
     title: "The Lion, the Witch, and the Wardrobe",
     author: "C.S. Lewis",
     image: "narnia.jpg",
     genre: "fantasy"
 },
 {
+    id: 20,
     title: "Jonathan Strange & Mr. Norrell",
     author: "Susanna Clarke",
     image: "jonathan-strange.jpg",
